eede48e1e92693aede6563488913d75d2d0060f2
717autoparts <717autoshop@gmail.com>
2026-06-01 23:48:32 -0400

    Update ebay.js

--- api/ebay.js
+++ api/ebay.js
@@ -9,14 +9,26 @@ export default async function handler(req, res) {
   try {
     const { endpoint, xml, headers } = req.body;
 
-    return res.status(200).json({
-      debug: true,
-      endpoint,
-      xmlLength: xml ? xml.length : 0,
-      xmlPreview: xml ? xml.substring(0, 100) : 'EMPTY',
-      headersReceived: Object.keys(headers || {})
+    const buf = Buffer.from(xml, 'utf8');
+
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/xml;charset=utf-8',
+        'Content-Length': buf.length.toString(),
+        'X-EBAY-API-COMPATIBILITY-LEVEL': headers['X-EBAY-API-COMPATIBILITY-LEVEL'],
+        'X-EBAY-API-CALL-NAME': headers['X-EBAY-API-CALL-NAME'],
+        'X-EBAY-API-APP-NAME': headers['X-EBAY-API-APP-NAME'],
+        'X-EBAY-API-DEV-NAME': headers['X-EBAY-API-DEV-NAME'],
+        'X-EBAY-API-CERT-NAME': headers['X-EBAY-API-CERT-NAME'],
+        'X-EBAY-API-SITEID': headers['X-EBAY-API-SITEID']
+      },
+      body: buf
     });
 
+    const text = await response.text();
+    res.setHeader('Content-Type', 'text/xml');
+    res.status(200).send(text);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
